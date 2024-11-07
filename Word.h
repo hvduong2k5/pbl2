b72181dd82f2521daf9e20ab9ec13766ebb95bee
HvDuong <hd2005555@gmail.com>
2024-11-07 12:41:22 +0700

    update file Word.h and Word.cpp add class

---
+++ Word.h
@@ -0,0 +1,19 @@
+#ifndef WORD_H
+#define WORD_H
+#include<string>
+using namespace std;
+class Word
+{
+private:
+    Word *left,*right;
+    string word;
+    string part_of_speech;
+    string *example_sentence;
+    string *meaning;
+    string ipa;
+public:
+    Word();
+    ~Word();
+};
+
+#endif
