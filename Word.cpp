1a1f5e02c697b58714793d10ca1efdc22cb30d1b
vcongggggg <vancong030809@gmail.com>
2024-11-08 21:09:22 +0700

    Update Word.cpp

--- Word.cpp
+++ Word.cpp
@@ -4,6 +4,40 @@ Word::Word()
 {
 }
 
+Word::Word(const std::string& line) : left(NULL), right(NULL) {
+    int i = 0;
+    std::string buffer;
+
+    while (line[i] != '@') buffer += line[i++];
+    i++;
+    tu = buffer;
+    buffer.clear();
+
+    while (line[i] != '@') buffer += line[i++];
+    i++;
+    loai = buffer;
+    buffer.clear();
+
+    while (line[i] != '@') buffer += line[i++];
+    i++;
+    dongnghia = buffer;
+    buffer.clear();
+
+    for (int j = 0; j < 3; ++j) {
+        while (line[i] != '@') buffer += line[i++];
+        i++;
+        nghia[j] = buffer;
+        buffer.clear();
+    }
+
+    for (int j = 0; j < 3; ++j) {
+        while (line[i] != '@') buffer += line[i++];
+        i++;
+        vidu[j] = buffer;
+        buffer.clear();
+    }
+}
+
 Word::~Word()
 {
 }
