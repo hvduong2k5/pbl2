d13988cb4ba94a859d5c70f03a6ac881a7b390dc
HvDuong <hd2005555@gmail.com>
2024-11-08 20:52:01 +0700

    update LinkedList

---
+++ LinkedList.h
@@ -0,0 +1,13 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+template<typename T> 
+class LinkedList
+{
+    private:
+    T *head,*tail;
+    public:
+    LinkedList();
+    ~LinkedList();
+    
+};
+#endif
