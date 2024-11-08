d509f523db205570a99acac046f0fb0f628e3042
vcongggggg <vancong030809@gmail.com>
2024-11-08 21:08:14 +0700

    Update Dictionary.cpp

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -4,41 +4,6 @@
 #include <windows.h>
 #include <conio.h>
 
-
-Word::Word(const std::string& line) : left(NULL), right(NULL) {
-    int i = 0;
-    std::string buffer;
-
-    while (line[i] != '@') buffer += line[i++];
-    i++;
-    tu = buffer;
-    buffer.clear();
-
-    while (line[i] != '@') buffer += line[i++];
-    i++;
-    loai = buffer;
-    buffer.clear();
-
-    while (line[i] != '@') buffer += line[i++];
-    i++;
-    dongnghia = buffer;
-    buffer.clear();
-
-    for (int j = 0; j < 3; ++j) {
-        while (line[i] != '@') buffer += line[i++];
-        i++;
-        nghia[j] = buffer;
-        buffer.clear();
-    }
-
-    for (int j = 0; j < 3; ++j) {
-        while (line[i] != '@') buffer += line[i++];
-        i++;
-        vidu[j] = buffer;
-        buffer.clear();
-    }
-}
-
 Dictionary::Dictionary() : capnhat(false), currentWord(NULL) {
     bang = new HashTable[26];
     for (int i = 0; i < 26; ++i) {
